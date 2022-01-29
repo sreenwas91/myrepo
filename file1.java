4b7520e448f849445791c49e93e810f1bc948051
Srinivas <srinivas@gmail.com>
2022-01-29 16:46:02 +0000

    my first commit of the source code

---
+++ file1.java
@@ -0,0 +1,9 @@
+total 3248
+-rw-rw-r-- 1 psrinivasgoud91 psrinivasgoud91       4 Jan 29 16:29 check_idle
+drwxr-xr-x 2 jenkins         jenkins            4096 Jan 29 15:00 hsperfdata_jenkins
+drwxr-xr-x 2 root            root               4096 Jan 29 15:01 hsperfdata_root
+drwx------ 2 jenkins         jenkins            4096 Jan 29 15:00 jetty-0_0_0_0-8080-war-_-any-4767627220535439232
+drwx------ 2 psrinivasgoud91 psrinivasgoud91    4096 Jan 29 15:27 ssh-NDBJqquCmnm5
+drwx------ 3 root            root               4096 Jan 29 15:00 systemd-private-a22176ccabc54ffb8692d2e626ce869e-rtkit-daemon.service-3WmnVq
+drwx------ 3 root            root               4096 Jan 29 15:00 systemd-private-a22176ccabc54ffb8692d2e626ce869e-systemd-timesyncd.service-newdn2
+-rw-r--r-- 1 jenkins         jenkins         3293397 Jan 29 15:00 winstone7405125700792219425.jar
---
+++ file2.java
@@ -0,0 +1,8 @@
+check_idle
+hsperfdata_jenkins
+hsperfdata_root
+jetty-0_0_0_0-8080-war-_-any-4767627220535439232
+ssh-NDBJqquCmnm5
+systemd-private-a22176ccabc54ffb8692d2e626ce869e-rtkit-daemon.service-3WmnVq
+systemd-private-a22176ccabc54ffb8692d2e626ce869e-systemd-timesyncd.service-newdn2
+winstone7405125700792219425.jar
