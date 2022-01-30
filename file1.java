c497093922960009c3050dfe9fb835b79a30cb36
sreenwas91 <98697594+sreenwas91@users.noreply.github.com>
2022-01-30 20:21:49 +0530

    updatning the web

--- file1.java
+++ file1.java
@@ -7,3 +7,4 @@ drwx------ 2 psrinivasgoud91 psrinivasgoud91    4096 Jan 29 15:27 ssh-NDBJqquCmn
 drwx------ 3 root            root               4096 Jan 29 15:00 systemd-private-a22176ccabc54ffb8692d2e626ce869e-rtkit-daemon.service-3WmnVq
 drwx------ 3 root            root               4096 Jan 29 15:00 systemd-private-a22176ccabc54ffb8692d2e626ce869e-systemd-timesyncd.service-newdn2
 -rw-r--r-- 1 jenkins         jenkins         3293397 Jan 29 15:00 winstone7405125700792219425.jar
+Am edit the text in the web 
